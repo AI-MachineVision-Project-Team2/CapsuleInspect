562a19f1e44b35b535f55d3fbe28b9e8b09b6d40
dasom <zlal324@gmail.com>
2025-08-20 16:47:57 +0900

    250820_수정

---
+++ CapsuleInspect/Teach/InspWindow.Visibility.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapsuleInspect.Teach
+{
+    public partial class InspWindow
+    {
+        public bool IgnoreInsp { get; set; }
+    }
+}
