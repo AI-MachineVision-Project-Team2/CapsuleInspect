a354fe31b4aff1bdf7787f1a43ada5f4766e4cc6
dasom <zlal324@gmail.com>
2025-08-25 13:36:20 +0900

    pdf보고서 추가하기

---
+++ CapsuleInspect/Util/ReportSession.cs
@@ -0,0 +1,22 @@
+﻿using CapsuleInspect.Core;
+using CapsuleInspect.Inspect;
+using CapsuleInspect.Teach;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.DocumentObjectModel.Tables;
+using MigraDoc.Rendering;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace CapsuleInspect.Util
+{
+    internal class ReportSession
+    {
+    }
+}
